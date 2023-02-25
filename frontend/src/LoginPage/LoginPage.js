09f4c845625fd464e738262f9d8f9b518e256286
Ming En <mingen8216@gmail.com>
2023-02-25 21:17:58 +0800

    login page done

--- frontend/src/LoginPage/LoginPage.js
+++ frontend/src/LoginPage/LoginPage.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import ChangePassword from "../components/FormComponents/ChangePassword";
+import CenteredText from "../components/Grid/CenteredText";
 import Form from "./Form";
+import NinjaIcon from "../assets/Ninja.svg";
+import LogoIcon from "../assets/Logo.svg";
 
 const LoginPage = () => {
   const [isChecked, setIsChecked] = useState(false);
@@ -13,10 +16,10 @@ const LoginPage = () => {
 
   const resetPasswordHandler = () => {
     return null;
-  }
+  };
 
   return (
-    <div className="flex justify-around bg-cover bg-login-page w-screen h-screen">
+    <div className="flex flex-col relative justify-evenly bg-cover bg-login-page w-screen h-screen">
       <div className="w-3/4 h-3/4 self-center rounded-3xl bg-red-100/80 flex flex-row justify-between divide-x-4">
         <Form title={"Login"}>
           <ChangePassword
@@ -27,6 +30,17 @@ const LoginPage = () => {
         </Form>
         <Form title={"Register"} />
       </div>
+      <div className="flex flex-row w-3/4 self-center justify-between">
+        <img src={LogoIcon} alt="NinjaVan" />
+      </div>
+      <p className="absolute bottom-10 right-10">
+        By logging in, you are agreeing to our Term and Privacy Policy
+      </p>
+      <img
+        src={NinjaIcon}
+        alt="NinjaIcon"
+        className="absolute bottom-10 left-10"
+      />
     </div>
   );
 };
